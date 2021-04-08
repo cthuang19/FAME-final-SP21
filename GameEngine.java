8c9d8afe90da8e88a35de3a11e8a93a53ee16dca
cthuang19 <cthuang2@illinois.edu>
2021-04-08 10:38:40 -0700

    add button

--- GameEngine.java
+++ GameEngine.java
@@ -9,6 +9,7 @@ import javafx.animation.*;
 import javafx.scene.input.KeyEvent;
 import javafx.event.*;
 import javafx.scene.paint.*;
+import javafx.scene.control.Button;
 
 import java.util.*;
 import java.io.*;
@@ -16,14 +17,25 @@ import java.applet.AudioClip;
 import java.beans.XMLEncoder;
 
 public class GameEngine extends Application {
+
+    public final int CANVAS_WIDTH = 1600;
+    public final int CANVAS_HEIGHT = 1200;
+    public final int BUTTON_WIDTH = 800;
+    public final int BUTTON_HEIGHT = 100;
+    
+    public boolean state;
     @Override
     public void start(Stage theStage) {
         Group root = new Group();
         Scene initial = new Scene(root);
         theStage.setScene(initial);
 
-        Canvas canvas_initial = new Canvas(1600, 1200);
+        Canvas canvas_initial = new Canvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+
+        Button english_button = new Button("English");
+        english_button.setMinSize(BUTTON_WIDTH, BUTTON_HEIGHT);
         root.getChildren().add(canvas_initial);
+        root.getChildren().add(english_button);
 
         GraphicsContext gc_initial = canvas_initial.getGraphicsContext2D();
 
@@ -32,5 +44,7 @@ public class GameEngine extends Application {
 
         theStage.show();
     }
+
+    
 
 }
